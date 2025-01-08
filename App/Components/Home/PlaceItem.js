d85e9ce00ba100486923cbf23a1f31c9995a402b
Rajveer Singh Khanduja <128909262+rajveerkhanduja@users.noreply.github.com>
2025-01-08 19:08:12 +0530

    Update PlaceItem.js

--- App/Components/Home/PlaceItem.js
+++ App/Components/Home/PlaceItem.js
@@ -21,7 +21,7 @@ export default function PlaceItem({ place }) {
           "?maxwidth=400" +
           "&photo_reference=" +
           place?.photos[0]?.photo_reference +
-          "&key=AIzaSyClQVMQ3tKZom-4ORyGP4ga5bfzNOLD5v4",
+          "&key=API_KEY",
         }}
         style={{ width: 110, height: 110, borderRadius: 15 }}
       />:
